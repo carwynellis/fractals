86fab9f543131128da7d0279b3b3e34306b18df4
Carwyn Ellis <carwynellis@gmail.com>
2020-12-21 17:41:43 +0000

    Test out image writing code

--- src/main/scala/fractals/Main.scala
+++ src/main/scala/fractals/Main.scala
@@ -1,6 +1,21 @@
 package fractals
 
-object Main extends App {
-  println(s"Hello world!")
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
+object Main extends App {
+
+  private val Width = 640
+  private val Height = 480
+
+  // Initial test bed for image writing code
+  try {
+    val image = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB)
+    val outputFile = new File("image.png")
+    ImageIO.write(image, "png", outputFile)
+  }
+  catch {
+    case e: Exception => println(s"Caught exception: $e")
+  }
 }
