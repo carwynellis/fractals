14f7527fe03fd9581a9e29984fd3b8994b5793f2
Carwyn Ellis <carwynellis@gmail.com>
2020-12-23 12:08:15 +0000

    Parallelise render and use lazy lists to reduce memory usage

--- build.sbt
+++ build.sbt
@@ -1,11 +1,11 @@
 name := "fractals"
-
 version := "0.1"
-
 scalaVersion := "2.13.4"
 
+val parallelCollectionsVersion = "1.0.0"
 val scalaTestVersion = "3.2.2"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  "org.scala-lang.modules" %% "scala-parallel-collections" % parallelCollectionsVersion,
+  "org.scalatest"          %% "scalatest"                  % scalaTestVersion % "test"
 )
--- src/main/scala/fractals/Main.scala
+++ src/main/scala/fractals/Main.scala
@@ -4,11 +4,12 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import scala.collection.parallel.CollectionConverters.ImmutableIterableIsParallelizable
 
 object Main extends App {
 
-  private val Width = 4000
-  private val Height = 4000
+  private val Width = 8000
+  private val Height = 8000
   private val PngFormat = "png" // TODO - are there constants for these?
   private val Iterations = 500
 
@@ -18,17 +19,18 @@ object Main extends App {
     height = Height
   )
 
-  // Initial test bed for image writing code
   try {
     val image = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB)
 
-    // Initial attempt at writing image data
-    val coords = for {
-      x <- 0 until Width
-      y <- 0 until Height
-    } yield(x, y)
+    println("Rendering image")
 
-    coords.foreach(p => image.setRGB(p._1, p._2, computeColor(p._1, p._2).getRGB))
+    (0 until Width).to(LazyList).foreach(x => {
+      (0 until Height).to(LazyList).par.foreach(y => {
+        image.setRGB(x, y, computeColor(x, y).getRGB)
+      })
+    })
+
+    println("Dome")
 
     val imageFile = new File("image.png")
     ImageIO.write(image, PngFormat, imageFile)
@@ -36,11 +38,6 @@ object Main extends App {
   catch {
     case e: Exception => println(s"Caught exception: $e")
   }
-
-//  private def computeColor(x: Int, y: Int) = {
-//    val intensity = Math.ceil((y/Height.toDouble)*255).toInt
-//    new Color(intensity, intensity, intensity)
-//  }
 
   private def computeColor(x: Int, y: Int) = {
     val iterations = mandelbrot.calculate(x, y)
